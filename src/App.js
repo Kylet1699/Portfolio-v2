1c204a1c78977c020216ce0b1dfed50a21c42cab
Kyle Tseng <kyletseng1699@gmail.com>
2024-11-14 20:49:12 -0800

    header and summary

--- src/App.js
+++ src/App.js
@@ -1,27 +1,48 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
+// Shadcn
+import { Tabs, TabsContent, TabsList, TabsTrigger } from './components/ui/tabs';
+
+// Components
+import About from './components/About/About';
+
 function App() {
+  const [tab, setTab] = useState('home');
+
+  useEffect(() => {
+    console.log(tab);
+  }, [tab]);
+
   return (
     <div className="App">
+      <header className="header">
+        <div className="header-left">
+          <span className="text-lg font-medium text-lightOrange">Kyle Tseng</span>
+          <span className="text-xs text-muted-foreground">Full Stack Developer</span>
+        </div>
+        <Tabs defaultValue="home" className="header-middle w-[400px]">
+          <TabsList>
+            <TabsTrigger value="home" onClick={() => setTab('home')}>
+              Home
+            </TabsTrigger>
+            <TabsTrigger value="Work" onClick={() => setTab('work')}>
+              Work
+            </TabsTrigger>
+          </TabsList>
+        </Tabs>
+        <div></div>
+      </header>
+
+      <About />
+      {/* 
       <h1>This is Ubuntu font in #cacaca</h1>
       <h2>This is also Ubuntu font in #cacaca</h2>
       <h3>This is DM Mono font in #cacaca</h3>
       <p>This is DM Mono font in #8a8a8a</p>
 
-      {/* You can also use the classes directly */}
       <div className="text-green font-ubuntu">Custom styled text</div>
-      <div className="text-body font-dm-mono">Custom styled text</div>
-
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <div className="text-body font-dm-mono">Custom styled text</div> */}
     </div>
   );
 }
---
+++ src/components/About/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './About.css';
+
+function About({ ...props }) {
+  return (
+    <div className="about-container text-left max-w-3xl m-auto pt-20 flex flex-col gap-2">
+      <div className="text-4xl font-medium">hi i'm kyle 😎</div>
+      <div className="text-4xl font-medium">
+        full stack developer based in <span className="text-lightOrange">vancouver</span>
+      </div>
+      <div className="text-xl text-body flex flex-row items-center gap-1.5">
+        <div class="circle"></div>
+        open to work
+      </div>
+    </div>
+  );
+}
+
+export default About;
